6f7ea56186224cfc6f459a5c1fc3d9258084e1be
Nathan Cheadle <natecheadle@gmail.com>
2025-02-13 22:18:26 -0800

    Added IRQ Handler to Cube Project

--- STM32/F303RE/Src/stm32f3xx_it.c
+++ STM32/F303RE/Src/stm32f3xx_it.c
@@ -199,6 +199,19 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
+  * @brief This function handles USART2 global interrupt / USART2 wake-up interrupt through EXTI line 26.
+  */
+void USART2_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART2_IRQn 0 */
+
+  /* USER CODE END USART2_IRQn 0 */
+  /* USER CODE BEGIN USART2_IRQn 1 */
+
+  /* USER CODE END USART2_IRQn 1 */
+}
+
+/**
   * @brief This function handles EXTI line[15:10] interrupts.
   */
 void EXTI15_10_IRQHandler(void)
